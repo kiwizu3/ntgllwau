900e1cef478ffb0dfa723088651c55d15c594000
Kavinda Sumanaweera <kavindasu@gmail.com>
2026-09-05 18:49:46 +0000

    Removed routes not needed

--- app/practice-areas/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import { practices } from "@/lib/content";
-
-export default async function LegacyPracticePage({ params }: { params: Promise<{ slug: string }> }) {
-  const { slug } = await params;
-  const practice = practices.find((item) => item.slug === slug);
-  redirect(practice ? `/${practice.slug}` : "/services");
-}
