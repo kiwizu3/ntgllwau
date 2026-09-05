ef3a9b6163e9fc1b90b889774bd120a129195427
kiwizu3 <kavindasu@gmail.com>
2026-09-05 02:10:00 -0400

    Security

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
+  trailingSlash: true,
   images: {
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 60 * 60 * 24 * 7,
